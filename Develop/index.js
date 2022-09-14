ca3ce6628d8ddb9895151abbca095a5afaa182fd
Vanese <vanesebuchanan@gmail.com>
2022-09-14 06:43:35 -0400

    Readme6

---
+++ Develop/index.js
@@ -0,0 +1,54 @@
+// TODO: Include packages needed for this application
+const fs = require("fs");
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown");
+
+// TODO: Create an array of questions for user input
+const questions = [
+    {
+        type: "input",
+        message: "What is project title?",
+        name: "title"
+    },
+    {
+        type: "input",
+        message: "Please provide a short description of your project",
+        name: "description",
+    },
+    {
+        type: "input",
+        message: "What command should be used in invoke dependencies?",
+        name: "installation",
+        default: "npm i",
+    },
+    {
+        type: "input",
+        message: "What is your email address?",
+        name: "email",
+    },
+    {
+        type: "input",
+        message: "What is your github username?",
+        name: "username",
+    }
+
+];
+
+//funtion to prompt user to answer questions
+const promptUser = () => {
+    return inquirer.prompt(questions)
+}
+
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+    return writeToFile(fileName, data);
+}
+
+// TODO: Create a function to initialize app
+function init() {
+
+}
+
+// Function call to initialize app
+init();
